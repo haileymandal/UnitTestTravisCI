df14ed319def925bffd5b4c110e3d8bcca9f02bf
Hailey Mandal <mandalhailey@gmail.com>
2023-06-07 12:22:45 -0700

    added first passing test case and imported assertion.*

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -1,8 +1,10 @@
+//arrange, action, assert
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -19,13 +21,13 @@ public class CILabTest {
     }
 
     @Test
-    public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
+    public void detectCapitalUse_NewObject_ReturnsFalse() {
+        //start with the action
+        boolean actual = myString.detectCapitalUse();
 
-    }
-    @Test
-    public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        //the test
+        assertFalse(actual);
+
     }
 
 
